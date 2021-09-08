1c28e31d4f3419e60bb05ae883bfd21f3121ab9f
AnthonyPiva <anthony-piva@hotmail.com>
2021-09-08 18:35:43 -0300

    Update caixa.js
    Retiradas as funções recursivas e feita implementação com laços de repetição.

--- JS/caixa.js
+++ JS/caixa.js
@@ -1,72 +1,51 @@
 const { exit } = require("process");
-const readline = require("readline");
+const question = require("prompt-sync")();
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-let notas = [1, 5, 10, 50, 100];
-var qtdNotas = [0, 0, 0, 0, 0];
+let notas = [200, 100, 50, 20, 10, 5, 1];
+var qtdNotas= [];
+var novoSaque, saque;
 var it=0;
-var novoSaque;
 
 function calcNotas(x){
-    for(var i=0;i<qtdNotas.length;i++)
+    for(var i=0;i<notas.length;i++)
         qtdNotas[i]=0;
     while(x>0){
-        if(x>=100){
-            x -= 100;
-            qtdNotas[4]++;
-        }else if(x>=50){
-            x -= 50;
-            qtdNotas[3]++;
-        }else if(x>=10){
-            x -= 10;
-            qtdNotas[2]++;
-        }else if(x>=5){
-            x -= 5;
-            qtdNotas[1]++;
-        }else if(x>=1){
-            x --;
-            qtdNotas[0]++;
+        for(var i=0;i<notas.length;i++){
+            if(x>=notas[i]){
+                x -= notas[i];
+                qtdNotas[i]++;
+                break;
+            }
         }
     }
     return qtdNotas;
 }
 
-var recursiveReadLine = function(){
-    rl.question("Olá Senhor(a), qual o valor do saque?\n", function(answer){
-        if(isNaN(answer)==false){
-            var x = parseInt(answer);
-            x = Math.abs(x);
-            console.log("Calculando a quantidade de notas..\n");
-            calcNotas(x);
-            it=0;
-            qtdNotas.forEach(function(i){
-                console.log("Notas de "+ notas[it] + ": " +i);
-                it++;
-            });
-            recursiveReadLine2();
-        }else{
-            console.log("\nSaque inválido, favor inserir um digito válido!");
-            recursiveReadLine();
-        }
+do{
+    console.log("Olá Senhor(a), qual o valor do saque?")
+    saque = question("> ");
+    while(isNaN(saque)){
+        console.log("Esse não é um valor válido! Por favor digite um número");
+        saque = question("> ");
+    }
+    var x = parseInt(saque);
+    x = Math.abs(x);
+    console.log("Calculando a quantidade de notas..\n");
+    calcNotas(x);
+    it=0;
+    qtdNotas.forEach(function(i){
+        console.log("Notas de "+ notas[it] + ": " +i);
+        it++;
     });
-}
 
-var recursiveReadLine2 = function(){
-    rl.question("\nDeseja fazer um novo saque?\nS para Sim e N para Não\n\n", function(answer2){
-        if(answer2=="N" || answer2=="n"){
-            console.log("Até mais!");
-            rl.close();
-        }else if(answer2=="S" || answer2=="s"){
-            recursiveReadLine();
-        }else{
-            console.log("Essa não é uma opção válida!")
-            recursiveReadLine2();
-        }
-    });
-}
+    console.log("Deseja sacar novamente?")
+    novoSaque = question("> ");
+    while(novoSaque != "N" && novoSaque !="n" && novoSaque != "S" && novoSaque != "s"){
+        console.log("Essa não é uma opção válida! Deseja sacar novamente?");
+        novoSaque = question("> ");
+    }
 
-recursiveReadLine();
+}while(novoSaque!='N' && novoSaque!="n");
+
+console.log("Até mais!!");
+return;
